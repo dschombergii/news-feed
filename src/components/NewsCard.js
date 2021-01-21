b6cd05bb5206fe772e1f859ae6a2399381069ccf
dschombergii <d.schombergii@gmail.com>
2021-01-21 15:30:17 -0600

    changed link to button

--- src/components/NewsCard.js
+++ src/components/NewsCard.js
@@ -25,12 +25,12 @@ const useStyles = makeStyles({
 
 export default function SimpleCard(props) {
   const classes = useStyles();
-  const {title, url, points, author, created_at} = props
+  const { title, url, points, author, created_at } = props
   const [isMinimized, setMinimized] = useState(false)
   const [isHidden, setHidden] = useState(false)
 
   function handleMinimizeClick() {
-    if(!isMinimized) {
+    if (!isMinimized) {
       setMinimized(true)
     } else {
       setMinimized(false)
@@ -38,74 +38,71 @@ export default function SimpleCard(props) {
   }
 
   function handleHiddenClick() {
-    if(!isHidden) {
+    if (!isHidden) {
       setHidden(true)
     } else {
       setHidden(false)
     }
   }
 
-  if(!isMinimized && !isHidden) {
+  if (!isMinimized && !isHidden) {
     return (
-      
-        <Card style={{display: 'flex', justifyContent: 'space-between', margin: '10px'}}className={classes.root}>
-        <CardContent style={{display: 'flex', flexDirection: 'column'}}>
-          <div style={{display: 'flex', justifyContent: 'flex-start'}}>
-            <Typography style={{display: 'flex', marginRight: '50px'}} variant="h5" component="h2">
-            {title}
+
+      <Card style={{ display: 'flex', justifyContent: 'space-between', margin: '10px' }} className={classes.root}>
+        <CardContent style={{ display: 'flex', flexDirection: 'column' }}>
+          <div style={{ display: 'flex', justifyContent: 'flex-start' }}>
+            <Typography style={{ display: 'flex', marginRight: '50px' }} variant="h5" component="h2">
+              {title}
             </Typography>
           </div>
-    
-          
-    
-          <div style={{display: 'flex', justifyContent: 'flex-start'}}>
-            <Typography style={{margin: '20px'}} className={classes.pos} color="textSecondary">
-            POINTS {points}
+
+
+
+          <div style={{ display: 'flex', justifyContent: 'flex-start' }}>
+            <Typography style={{ margin: '20px' }} className={classes.pos} color="textSecondary">
+              POINTS {points}
             </Typography>
-            <Typography style={{margin: '20px'}} className={classes.pos} color="textSecondary">
-            AUTHOR {author}
+            <Typography style={{ margin: '20px' }} className={classes.pos} color="textSecondary">
+              AUTHOR {author}
             </Typography>
-            <Typography style={{margin: '20px'}} className={classes.pos} color="textSecondary">
-            CREATED_AT {created_at}
+            <Typography style={{ margin: '20px' }} className={classes.pos} color="textSecondary">
+              CREATED_AT {created_at}
             </Typography>
           </div>
-    
-          
-          
-          
+
+
+
+
         </CardContent>
-        <div style={{marginLeft: '5px'}}>
-          <Typography variant="h5" component="h2">
-              <a style={{fontSize: '60%'}} href={url} target="_blank">GO TO ARTICLE</a>
-            
-            </Typography>
-          </div>
+        <div style={{ marginLeft: '5px' }}>
+        </div>
         <CardActions>
-          <Button style={{border: '1px solid red'}} size="small" onClick={handleMinimizeClick}>View Later</Button>
-          <Button style={{border: '1px solid red'}} size="small" onClick={handleHiddenClick}>Not Interested</Button>
+          <Button style={{ border: '1px solid green' }} size="small" onClick={handleMinimizeClick}><a style={{ textDecoration: "none" }} href={url} target="_blank">GO TO ARTICLE</a></Button>
+          <Button style={{ border: '1px solid blue' }} size="small" onClick={handleMinimizeClick}>View Later</Button>
+          <Button style={{ border: '1px solid red' }} size="small" onClick={handleHiddenClick}>Not Interested</Button>
         </CardActions>
       </Card>
     )
-        
-    
-  } else if(isMinimized && !isHidden) {
+
+
+  } else if (isMinimized && !isHidden) {
     return (
-      <Card style={{display: 'flex', justifyContent: 'space-between', margin: '10px'}}className={classes.root}>
-        <CardContent style={{display: 'flex', flexDirection: 'column'}}>
-        <div style={{display: 'flex', justifyContent: 'flex-start'}}>
-            <Typography style={{display: 'flex', marginRight: '50px'}} variant="h5" component="h2">
-            {title}
+      <Card style={{ display: 'flex', justifyContent: 'space-between', margin: '10px' }} className={classes.root}>
+        <CardContent style={{ display: 'flex', flexDirection: 'column' }}>
+          <div style={{ display: 'flex', justifyContent: 'flex-start' }}>
+            <Typography style={{ display: 'flex', marginRight: '50px' }} variant="h5" component="h2">
+              {title}
             </Typography>
           </div>
-    
-          
-          
-          
+
+
+
+
         </CardContent>
-        
+
         <CardActions>
-          <Button style={{border: '1px solid green'}} size="small" onClick={handleMinimizeClick}>View Now</Button>
-          <Button style={{border: '1px solid red'}} size="small" onClick={handleHiddenClick}>Not Interested</Button>
+          <Button style={{ border: '1px solid green' }} size="small" onClick={handleMinimizeClick}>View Now</Button>
+          <Button style={{ border: '1px solid red' }} size="small" onClick={handleHiddenClick}>Not Interested</Button>
         </CardActions>
       </Card>
     )
