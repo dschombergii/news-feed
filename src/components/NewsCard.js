0c802204835f4d87bc93ca1c93c8878a1ffaf2a5
rockman4417 <67634648+rockman4417@users.noreply.github.com>
2021-01-21 15:06:24 -0600

    added target blank

--- src/components/NewsCard.js
+++ src/components/NewsCard.js
@@ -76,7 +76,7 @@ export default function SimpleCard(props) {
         </CardContent>
         <div style={{marginLeft: '5px'}}>
           <Typography variant="h5" component="h2">
-              <a style={{fontSize: '60%'}} href={url}>GO TO ARTICLE</a>
+              <a style={{fontSize: '60%'}} href={url} target="_blank">GO TO ARTICLE</a>
             
             </Typography>
           </div>
